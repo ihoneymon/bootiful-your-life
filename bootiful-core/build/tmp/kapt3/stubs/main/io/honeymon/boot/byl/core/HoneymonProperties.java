9d18f2a922acf8eaf128c611a15c5166dfcc0459
honeymon <ihoneymon@gmail.com>
2019-04-16 12:10:28 +0900

    머맅 프로젝트 구성 성공 1

--- bootiful-core/build/tmp/kapt3/stubs/main/io/honeymon/boot/byl/core/HoneymonProperties.java
+++ bootiful-core/build/tmp/kapt3/stubs/main/io/honeymon/boot/byl/core/HoneymonProperties.java
@@ -5,10 +5,10 @@ import java.lang.System;
 /**
  * * Component 는 Configuration 클래스에서 불러오는 곳이 없는 경우 선언
  */
-@kotlin.Metadata(mv = {1, 1, 13}, bv = {1, 0, 3}, k = 1, d1 = {"\u0000\u0014\n\u0002\u0018\u0002\n\u0002\u0010\u0000\n\u0002\b\u0002\n\u0002\u0010\u000e\n\u0002\b\u000e\b\u0007\u0018\u00002\u00020\u0001B\u0005\u00a2\u0006\u0002\u0010\u0002R\u001a\u0010\u0003\u001a\u00020\u0004X\u0086.\u00a2\u0006\u000e\n\u0000\u001a\u0004\b\u0005\u0010\u0006\"\u0004\b\u0007\u0010\bR\u001a\u0010\t\u001a\u00020\u0004X\u0086.\u00a2\u0006\u000e\n\u0000\u001a\u0004\b\n\u0010\u0006\"\u0004\b\u000b\u0010\bR\u001a\u0010\f\u001a\u00020\u0004X\u0086.\u00a2\u0006\u000e\n\u0000\u001a\u0004\b\r\u0010\u0006\"\u0004\b\u000e\u0010\bR\u001a\u0010\u000f\u001a\u00020\u0004X\u0086.\u00a2\u0006\u000e\n\u0000\u001a\u0004\b\u0010\u0010\u0006\"\u0004\b\u0011\u0010\b\u00a8\u0006\u0012"}, d2 = {"Lio/honeymon/boot/byl/core/HoneymonProperties;", "", "()V", "age", "", "getAge", "()Ljava/lang/String;", "setAge", "(Ljava/lang/String;)V", "email", "getEmail", "setEmail", "homesite", "getHomesite", "setHomesite", "name", "getName", "setName", "bootiful-core"})
+@kotlin.Metadata(mv = {1, 1, 11}, bv = {1, 0, 2}, k = 1, d1 = {"\u0000\u0014\n\u0002\u0018\u0002\n\u0002\u0010\u0000\n\u0002\b\u0002\n\u0002\u0010\u000e\n\u0002\b\u000e\b\u0017\u0018\u00002\u00020\u0001B\u0005\u00a2\u0006\u0002\u0010\u0002R\u001a\u0010\u0003\u001a\u00020\u0004X\u0096.\u00a2\u0006\u000e\n\u0000\u001a\u0004\b\u0005\u0010\u0006\"\u0004\b\u0007\u0010\bR\u001a\u0010\t\u001a\u00020\u0004X\u0096.\u00a2\u0006\u000e\n\u0000\u001a\u0004\b\n\u0010\u0006\"\u0004\b\u000b\u0010\bR\u001a\u0010\f\u001a\u00020\u0004X\u0096.\u00a2\u0006\u000e\n\u0000\u001a\u0004\b\r\u0010\u0006\"\u0004\b\u000e\u0010\bR\u001a\u0010\u000f\u001a\u00020\u0004X\u0096.\u00a2\u0006\u000e\n\u0000\u001a\u0004\b\u0010\u0010\u0006\"\u0004\b\u0011\u0010\b\u00a8\u0006\u0012"}, d2 = {"Lio/honeymon/boot/byl/core/HoneymonProperties;", "", "()V", "age", "", "getAge", "()Ljava/lang/String;", "setAge", "(Ljava/lang/String;)V", "email", "getEmail", "setEmail", "homesite", "getHomesite", "setHomesite", "name", "getName", "setName", "bootiful-core"})
 @org.springframework.boot.context.properties.ConfigurationProperties(prefix = "io.honeymon")
 @org.springframework.stereotype.Component()
-public final class HoneymonProperties {
+public class HoneymonProperties {
     @org.jetbrains.annotations.NotNull()
     public java.lang.String name;
     @org.jetbrains.annotations.NotNull()
@@ -19,38 +19,38 @@ public final class HoneymonProperties {
     public java.lang.String homesite;
     
     @org.jetbrains.annotations.NotNull()
-    public final java.lang.String getName() {
+    public java.lang.String getName() {
         return null;
     }
     
-    public final void setName(@org.jetbrains.annotations.NotNull()
+    public void setName(@org.jetbrains.annotations.NotNull()
     java.lang.String p0) {
     }
     
     @org.jetbrains.annotations.NotNull()
-    public final java.lang.String getAge() {
+    public java.lang.String getAge() {
         return null;
     }
     
-    public final void setAge(@org.jetbrains.annotations.NotNull()
+    public void setAge(@org.jetbrains.annotations.NotNull()
     java.lang.String p0) {
     }
     
     @org.jetbrains.annotations.NotNull()
-    public final java.lang.String getEmail() {
+    public java.lang.String getEmail() {
         return null;
     }
     
-    public final void setEmail(@org.jetbrains.annotations.NotNull()
+    public void setEmail(@org.jetbrains.annotations.NotNull()
     java.lang.String p0) {
     }
     
     @org.jetbrains.annotations.NotNull()
-    public final java.lang.String getHomesite() {
+    public java.lang.String getHomesite() {
         return null;
     }
     
-    public final void setHomesite(@org.jetbrains.annotations.NotNull()
+    public void setHomesite(@org.jetbrains.annotations.NotNull()
     java.lang.String p0) {
     }
     
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,86 +1,18 @@
-plugins {
-    id("org.springframework.boot") version "2.1.4.RELEASE"
-    id("org.jetbrains.kotlin.jvm") version "1.3.21"
-    id("org.jetbrains.kotlin.plugin.spring") version "1.3.21"
-    id("org.jetbrains.kotlin.kapt") version "1.3.21"
-}
-
-subprojects {
-    apply {
-        plugin("kotlin")
-        plugin("kotlin-kapt")
-        plugin("io.spring.dependency-management")
-    }
-
-    group = "io.honeymon.boot"
-    version = "1.0.0"
-
-    repositories {
-        mavenCentral()
-    }
-
-    dependencies {
-        compile("com.fasterxml.jackson.module:jackson-module-kotlin")
-        compile("org.jetbrains.kotlin:kotlin-reflect")
-        compile("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-        compile("org.springframework.boot:spring-boot-starter-logging")
-
-        /**
-         * @see <a href="https://kotlinlang.org/docs/reference/kapt.html">Annotation Processing with Kotlin</a>
-         */
-        kapt("org.springframework.boot:spring-boot-configuration-processor")
-        compileOnly("org.springframework.boot:spring-boot-configuration-processor")
-
-        testCompile("org.springframework.boot:spring-boot-starter-test")
-    }
-
-    tasks {
-        compileKotlin {
-            kotlinOptions {
-                freeCompilerArgs = listOf("-Xjsr305=strict")
-                jvmTarget = "1.8"
-            }
-            dependsOn(processResources) // kotlin 에서 ConfigurationProperties
-        }
-
-
-        compileTestKotlin {
-            kotlinOptions {
-                freeCompilerArgs = listOf("-Xjsr305=strict")
-                jvmTarget = "1.8"
-            }
-        }
-    }
-}
-
-project("bootiful-core") {
-
-    dependencies {
-        compile("org.springframework.boot:spring-boot-starter-data-jpa")
-
-        runtimeOnly("com.h2database:h2")
-    }
-}
-
-project(":bootiful-sbadmin") {
-
-
-    dependencies {
-        compile(project(":bootiful-core"))
-
-        compile("de.codecentric:spring-boot-admin-starter-server:2.1.4")
-
-        // Spring Boot Admin-api 대신 클라우드 환경에서 사용한다면!
-//        compile("org.springframework.cloud:spring-cloud-starter-netflix-eureka-api")
-    }
-}
-
-project("bootiful-api") {
-    dependencies {
-        compile(project(":bootiful-core"))
-
-        compile("org.springframework.boot:spring-boot-starter-web")
-        compile("org.springframework.boot:spring-boot-starter-security")
-        compile("de.codecentric:spring-boot-admin-starter-client:2.1.4")
-    }
-}
+/**
+ * @see <a href="https://docs.gradle.org/current/userguide/kotlin_dsl.html#sec:multi_project_builds">Multi project</a>
+ */
+//plugins {
+//    id("org.jetbrains.kotlin.jvm") version "1.2.71"
+//    id("org.jetbrains.kotlin.plugin.spring") version "1.2.71"
+//    id("org.jetbrains.kotlin.kapt") version "1.2.71"
+//    id("org.springframework.boot") version "2.1.4.RELEASE"
+//}
+//
+//group = "io.honeymon.boot.byl"
+//version = "1.0.0"
+//
+//project(":bootiful-core") {}
+//
+//project(":bootiful-api") {}
+//
+//project(":bootiful-sbadmin") {}
